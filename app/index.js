53d27198ae7fbada0e719084476e774b9943eda7
Adrian Quevedo <adriancquevedo@sanchobbdo.com.co>
2013-10-09 19:05:06 -0500

    add basic configuration prompts and folder structure
    add promts to:
    - Get Name of the Site
    - Confirm inclusion of responsive HTML tags
    - Confirm inclusion of Open Graph Tags
    
    generate basic file structure

--- app/index.js
+++ app/index.js
@@ -5,46 +5,73 @@ var yeoman = require('yeoman-generator');
 
 
 var SanchobbdoGenerator = module.exports = function SanchobbdoGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
+    this.on('end', function () {
+        this.installDependencies({ skipInstall: options['skip-install'] });
+    });
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(SanchobbdoGenerator, yeoman.generators.Base);
 
 SanchobbdoGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+    var cb = this.async();
 
-  // have Yeoman greet the user.
-  console.log(this.yeoman);
+    // have Yeoman greet the user.
+    console.log(this.yeoman);
+    console.log('This is Sancho BBDO Front-end Project Generator');
+    // console.log('Out of the box this include SASS support for Bourbon/Neat and jQuery.');
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
-  }];
+    var prompts = [{
+        name: 'siteName',
+        message: 'What do you want to call the site?'
+    },
+    {
+        type: 'confirm',
+        name: 'siteResponsive',
+        message: 'Would you like to include HTML tags for a responsive site?',
+        default: true
+    },
+    {
+        type: 'confirm',
+        name: 'openGraph',
+        message: 'Would you like to include HTML tags for Open Graph?',
+        default: true
+    }];
 
-  this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+    this.prompt(prompts, function (props) {
+        this.siteName = props.siteName;
+        this.siteResponsive = props.siteResponsive;
+        this.openGraph = props.openGraph;
 
-    cb();
-  }.bind(this));
+        cb();
+    }.bind(this));
 };
 
 SanchobbdoGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
+    this.mkdir('app');
+    this.mkdir('app/assets');
+    this.directory('css','app/assets/css');
+    this.mkdir('app/assets/js');
+    this.mkdir('app/assets/i');
+    this.mkdir('app/partials');
+    this.copy('partial1.html', 'app/partials/partial1.html');
+    this.copy('partial2.html', 'app/partials/partial2.html');
 
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+    this.copy('apple-touch-icon-precomposed.png', 'apple-touch-icon-precomposed.png');
+    this.copy('favicon.ico', 'favicon.ico');
+
+    this.template('humans.txt', 'humans.txt');
+
+    this.template('index.html', 'index.html');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
 };
 
 SanchobbdoGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
 };
